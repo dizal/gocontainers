bb61c5e566ed0489564d9a23f00a97da7f5dc694
DiZaL <4dizal@gmail.com>
2020-09-13 05:35:55 +0300

    test coverage

--- set/set_test.go
+++ set/set_test.go
@@ -37,7 +37,11 @@ func TestSetDelete(t *testing.T) {
 	s.Add("value")
 	s.Add("value2")
 
-	s.Delete("value2")
+	ok := s.Delete("value2")
+
+	if !ok {
+		t.Error("delete: delete status is false")
+	}
 
 	if s.Contain("value2") {
 		t.Error("delete: value is contain")
@@ -45,6 +49,12 @@ func TestSetDelete(t *testing.T) {
 
 	if s.Len() != 1 {
 		t.Error("delete: Set length is not 1")
+	}
+
+	ok = s.Delete("value2")
+
+	if ok {
+		t.Error("delete: delete status is true")
 	}
 }
 
@@ -64,6 +74,48 @@ func TestSetErase(t *testing.T) {
 	if s.Len() != 0 {
 		t.Error("erase: set length is not null")
 	}
+}
+
+func TestSetSlice(t *testing.T) {
+	s := New()
+	s.Add("value1")
+	s.Add("value2")
+
+	slice := s.ToSlice()
+
+	if len(slice) != 2 {
+		t.Error("slice: slice length must be 2")
+	}
+}
+
+func TestSetString(t *testing.T) {
+	s := New()
+
+	if s.String() != "SET<>" {
+		t.Errorf("string: Expected SET<>. Received %s", s.String())
+	}
+
+	s.Add("v1")
+	s.Add("v2")
+
+	if s.String() != "SET<v1, v2>" {
+		t.Errorf("string: Expected SET<v1, v2>. Received %s", s.String())
+	}
+}
+
+func TestSetEach(t *testing.T) {
+	s := New()
+
+	s.Add("v1")
+	s.Add("v2")
+	s.Add("v3")
+
+	s.Each(func(v interface{}) bool {
+		if v != "v1" && v != "v2" && v != "v3" {
+			t.Errorf("Each: Expected 'v1' or 'v2' or 'v3'. Received '%s'", v)
+		}
+		return true
+	})
 }
 
 func BenchmarkSetAdd(b *testing.B) {
